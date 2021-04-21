8026b7550c480f5905a8a40822734d8276ad9945
Andrew De Ponte <cyphactor@gmail.com>
2021-04-21 10:07:03 -0400

    Generalize prefix(while:) parser
    So that it works with generic Collections not just Substrings.
    
    [changelog]
    changed: .prefix(while:) parser to work on generic collections

--- Sources/FunctionalParserKit/PrefixWhileParser.swift
+++ Sources/FunctionalParserKit/PrefixWhileParser.swift
@@ -1,5 +1,8 @@
-extension Parser where Input == Substring, Output == Substring {
-    public static func prefix(while p: @escaping (Character) -> Bool) -> Self {
+extension Parser where Input: Collection,
+                       Input.SubSequence == Input,
+                       Input.Element: Equatable,
+                       Output == Input.SubSequence {
+    public static func prefix(while p: @escaping (Input.Element) -> Bool) -> Self {
         Self { input in
             let output = input.prefix(while: p)
             input.removeFirst(output.count)
--- Tests/FunctionalParserKitTests/PrefixWhileParserTests.swift
+++ Tests/FunctionalParserKitTests/PrefixWhileParserTests.swift
@@ -3,12 +3,19 @@ import FunctionalParserKit
 
 final class PrefixWhileParserTests: XCTestCase {
     func testNormalCase() {
-        let res = Parser.prefix(while: { $0 != "," }).run("Hello, Bob")
+        let res = Parser<Substring, Substring>.prefix(while: { $0 != "," }).run("Hello, Bob")
         XCTAssertEqual(res.match!, "Hello")
         XCTAssertEqual(res.rest, ", Bob")
     }
 
+    func testOnArraySlice() {
+        let res = Parser<ArraySlice<String>, ArraySlice<String>>.prefix(while: { $0 != "World" }).run(["Hello", "World", "Bob"][...])
+        XCTAssertEqual(res.match!, ["Hello"])
+        XCTAssertEqual(res.rest, ["World", "Bob"])
+    }
+
     static var allTests = [
         ("testNormalCase", testNormalCase),
+        ("testOnArraySlice", testOnArraySlice)
     ]
 }
